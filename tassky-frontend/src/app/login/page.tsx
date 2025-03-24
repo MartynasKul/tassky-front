18c28ee1e05e24843d8b7c2df963714d71eafb86
Martynas Kuliešius <martiss200@gmail.com>
2025-03-25 00:16:40 +0200

    setup: initial pages

---
+++ tassky-frontend/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function Login() {
+
+  return (
+    <body>
+      <div className='bg-blue-800 justify-center'>
+        <text className='font-bold text-white'>
+          Login
+        </text>
+      </div>
+    </body>
+  );
+}
