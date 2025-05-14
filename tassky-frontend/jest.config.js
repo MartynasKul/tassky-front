e1b928ba7b932a2473bf17639cf3b756c6ae08f1
Martynas Kuliešius <martiss200@gmail.com>
2025-05-14 20:09:32 +0300

    fixes?

--- tassky-frontend/jest.config.js
+++ tassky-frontend/jest.config.js
@@ -1,25 +1,18 @@
-// jest.config.js - Updated to use babel-jest for tests only
-/** @type {import('jest').Config} */
-const config = {
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
-    // Updated paths for your specific project structure with src directory
     '^@/app/(.*)$': '<rootDir>/src/app/$1',
     '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
-  transform: {
-    // Use babel-jest for test transformations only
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest'],
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  testMatch: [
-    '**/__test__/**/*.test.{js,jsx,ts,tsx}',
-    '**/?(*.)+(spec|test).{js,jsx,ts,tsx}',
-  ],
-  roots: ['<rootDir>/src', '<rootDir>/__test__'],
-};
+}
 
-module.exports = config;
+module.exports = createJestConfig(customJestConfig)
